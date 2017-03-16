3551aafc9cfeae9ffab961774ec41a72e16b6513
Henrique Silva <hrzsweb@gmail.com>
2017-03-16 11:50:58 -0300

    Update Classify_URL_Image.py

--- Exemples/Classify_URL_Image.py
+++ Exemples/Classify_URL_Image.py
@@ -4,7 +4,7 @@ import json
 from watson_developer_cloud import VisualRecognitionV3
 
 # Configuracao da API
-VR = VisualRecognitionV3(version='2016-05-20', api_key = '9b367cc730c4e57599441b0b9b01bb612eb25a1a' )
+VR = VisualRecognitionV3(version='2016-05-20', api_key = 'YOUR_API_KEY_HERE' )
 
 #URL da imagem a ser classificada
 url_image='http://3.bp.blogspot.com/-7Tl5bSuCbIk/VPPEvB_oJYI/AAAAAAAAAVE/n9aVLWKhxWo/s1600/locomotiva.jpg'
